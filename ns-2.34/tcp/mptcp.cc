0e241230dd82d46e2d06108a58956ce348acfe3d
yoshifumi.nishida <yoshifumi.nishida@19c7de68-0d20-afff-497f-5bc54dec554c>
2010-05-13 03:14:17 +0000

    initial release

---
+++ ns-2.34/tcp/mptcp.cc
@@ -0,0 +1,376 @@
+/*
+ * Copyright (C) 2010 WIDE Project.  All rights reserved.
+ *
+ * Yoshifumi Nishida  <nishida@sfc.wide.ad.jp>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the project nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE PROJECT AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE PROJECT OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include "ip.h"
+#include "flags.h"
+#include "random.h"
+#include "template.h"
+#include "mptcp.h"
+
+static class MptcpClass:public TclClass
+{
+public:
+  MptcpClass ():TclClass ("Agent/MPTCP")
+  {
+  }
+  TclObject *create (int, const char *const *)
+  {
+    return (new MptcpAgent ());
+  }
+}
+
+class_mptcp;
+
+MptcpAgent::MptcpAgent ():Agent (PT_TCP), sub_num_ (0), total_bytes_ (0),
+mcurseq_ (1), mackno_ (0)
+{
+}
+
+void
+MptcpAgent::delay_bind_init_all ()
+{
+  Agent::delay_bind_init_all ();
+}
+
+/* haven't implemented yet */
+#if 0
+int
+MptcpAgent::delay_bind_dispatch (const char *cpVarName,
+                                 const char *cpLocalName,
+                                 TclObject * opTracer)
+{
+  return Agent::delay_bind_dispatch (cpVarName, cpLocalName, opTracer);
+}
+
+void
+MptcpAgent::TraceAll ()
+{
+}
+
+void
+MptcpAgent::TraceVar (const char *cpVar)
+{
+}
+
+void
+MptcpAgent::trace (TracedVar * v)
+{
+  if (eTraceAll == TRUE)
+    TraceAll ();
+  else
+    TraceVar (v->name ());
+}
+#endif
+
+int
+MptcpAgent::command (int argc, const char *const *argv)
+{
+  if (argc == 2) {
+    if (strcmp (argv[1], "listen") == 0) {
+      for (int i = 0; i < sub_num_; i++) {
+        if (subflows_[i].tcp_->command (argc, argv) != TCL_OK)
+          return (TCL_ERROR);
+      }
+      return (TCL_OK);
+    }
+    if (strcmp (argv[1], "reset") == 0) {
+      for (int i = 0; i < sub_num_; i++) {
+        for (int j = 0; j < dst_num_; j++) {
+          if (check_routable (i, dsts_[j].addr_, dsts_[j].port_)) {
+            subflows_[i].daddr_ = dsts_[j].addr_;
+            subflows_[i].dport_ = dsts_[j].port_;
+            subflows_[i].tcp_->daddr () = dsts_[j].addr_;
+            subflows_[i].tcp_->dport () = dsts_[j].port_;
+            break;
+          }
+        }
+      }
+      subflows_[0].tcp_->mptcp_set_primary ();
+      return (TCL_OK);
+    }
+  }
+  if (argc == 3) {
+    if (strcmp (argv[1], "attach-tcp") == 0) {
+      int id = get_subnum ();
+      subflows_[id].tcp_ = (MpFullTcpAgent *) TclObject::lookup (argv[2]);
+      subflows_[id].used = true;
+      subflows_[id].addr_ = subflows_[id].tcp_->addr ();
+      subflows_[id].port_ = subflows_[id].tcp_->port ();
+      subflows_[id].tcp_->mptcp_set_core (this);
+      sub_num_++;
+      return (TCL_OK);
+    }
+    else if (strcmp (argv[1], "set-multihome-core") == 0) {
+      core_ = (Classifier *) TclObject::lookup (argv[2]);
+      if (core_ == NULL) {
+        return (TCL_ERROR);
+      }
+      return (TCL_OK);
+    }
+  }
+  if (argc == 4) {
+    if (strcmp (argv[1], "add-multihome-destination") == 0) {
+      add_destination (atoi (argv[2]), atoi (argv[3]));
+      return (TCL_OK);
+    }
+  }
+  if (argc == 6) {
+    if (strcmp (argv[1], "add-multihome-interface") == 0) {
+      /* argv[2] indicates the addresses of the mptcp session */
+
+      /* find the id for tcp bound to this address */
+      int id = find_subflow (atoi (argv[2]));
+      if (id < 0) {
+        fprintf (stderr, "cannot find tcp bound to interface addr [%s]",
+                 argv[2]);
+        return (TCL_ERROR);
+      }
+      subflows_[id].tcp_->port () = atoi (argv[3]);
+      subflows_[id].port_ = atoi (argv[3]);
+      subflows_[id].target_ = (NsObject *) TclObject::lookup (argv[4]);
+      subflows_[id].link_ = (NsObject *) TclObject::lookup (argv[5]);
+      if (subflows_[id].target_ == NULL || subflows_[id].link_ == NULL)
+        return (TCL_ERROR);
+
+      return (TCL_OK);
+    }
+  }
+  return (Agent::command (argc, argv));
+}
+
+int
+MptcpAgent::get_subnum ()
+{
+  for (int i = 0; i < MAX_SUBFLOW; i++) {
+    if (!subflows_[i].used)
+      return i;
+  }
+  return -1;
+}
+
+int
+MptcpAgent::find_subflow (int addr, int port)
+{
+  for (int i = 0; i < MAX_SUBFLOW; i++) {
+    if (subflows_[i].addr_ == addr && subflows_[i].port_ == port)
+      return i;
+  }
+  return -1;
+}
+
+int
+MptcpAgent::find_subflow (int addr)
+{
+  for (int i = 0; i < MAX_SUBFLOW; i++) {
+    if (subflows_[i].addr_ == addr)
+      return i;
+  }
+  return -1;
+}
+
+void
+MptcpAgent::recv (Packet * pkt, Handler * h)
+{
+  hdr_ip *iph = hdr_ip::access (pkt);
+  hdr_tcp *tcph = hdr_tcp::access (pkt);
+
+  /* find subflow tcp from the destination address */
+  int id = find_subflow (iph->daddr ());
+  if (id < 0) {
+    fprintf (stderr,
+             "MptcpAgent:recv() fatal error. cannot find destination\n");
+    abort ();
+  }
+
+  /* processing mptcp options */
+  if (tcph->mp_capable ()) {
+    subflows_[id].tcp_->mpcapable_ = true;
+  }
+  if (tcph->mp_join ()) {
+    subflows_[id].tcp_->mpjoin_ = true;
+  }
+  if (tcph->mp_ack ()) {
+    subflows_[id].tcp_->mptcp_remove_mapping (tcph->mp_ack ());
+  }
+
+  if (tcph->mp_dsn ()) {
+    subflows_[id].tcp_->mpack_ = true;
+    subflows_[id].tcp_->mptcp_recv_add_mapping (tcph->mp_dsn (),
+                                                tcph->mp_subseq (),
+                                                tcph->mp_dsnlen ());
+  }
+  subflows_[id].tcp_->mptcp_set_byteacked (pkt);
+
+  /* make sure packet will be return to the src addr of the packet */
+  subflows_[id].tcp_->daddr () = iph->saddr ();
+  subflows_[id].tcp_->dport () = iph->sport ();
+
+  /* call subflow's recv function */
+  subflows_[id].tcp_->recv (pkt, h);
+  send_control ();
+}
+
+/*
+ * add possible destination address
+ */
+void
+MptcpAgent::add_destination (int addr, int port)
+{
+  for (int i = 0; i < MAX_SUBFLOW; i++) {
+    if (dsts_[i].active_)
+      continue;
+    dsts_[i].addr_ = addr;
+    dsts_[i].port_ = port;
+    dsts_[i].active_ = true;
+    dst_num_++;
+    return;
+  }
+  fprintf (stderr, "fatal error. cannot add destination\n");
+  abort ();
+}
+
+/*
+ * check if this subflow can reach to the specified address
+ */
+bool MptcpAgent::check_routable (int sid, int addr, int port)
+{
+  Packet *
+    p = allocpkt ();
+  hdr_ip *
+    iph = hdr_ip::access (p);
+  iph->daddr () = addr;
+  iph->dport () = port;
+  bool
+    result = (static_cast < Classifier * >
+              (subflows_[sid].target_)->classify (p) > 0) ? true : false;
+  Packet::free (p);
+
+  return result;
+}
+
+void
+MptcpAgent::sendmsg (int nbytes, const char * /*flags */ )
+{
+  if (nbytes == -1)
+    total_bytes_ = TCP_MAXSEQ;
+  else
+    total_bytes_ = nbytes;
+  send_control ();
+}
+
+/*
+ * control sending data
+ */
+void
+MptcpAgent::send_control ()
+{
+  if (total_bytes_ > 0) {
+    /* one round */
+    bool slow_start = false;
+    for (int i = 0; i < sub_num_; i++) {
+
+      int mss = subflows_[i].tcp_->size ();
+      int cwnd = subflows_[i].tcp_->mptcp_get_cwnd () * mss;
+      int ssthresh = subflows_[i].tcp_->mptcp_get_ssthresh () * mss;
+      int maxseq = subflows_[i].tcp_->mptcp_get_maxseq ();
+      int highest_ack = subflows_[i].tcp_->mptcp_get_highest_ack ();
+      /* too naive logic to calculate outstanding bytes? */
+      int outstanding = maxseq - highest_ack;
+
+      if (cwnd < ssthresh) {
+        /* allow only one subflow to do slow start at the same time */
+        if (!slow_start) {
+          slow_start = true;
+          subflows_[i].tcp_->mptcp_set_slowstart (true);
+        }
+        else
+#if 1
+          subflows_[i].tcp_->mptcp_set_slowstart (false);
+#else
+          /* allow to do slow-start simultaneously */
+          subflows_[i].tcp_->mptcp_set_slowstart (true);
+#endif
+      }
+
+      int sendbytes = cwnd - outstanding;
+      if (sendbytes < mss)
+        continue;
+      if (sendbytes > total_bytes_)
+        sendbytes = total_bytes_;
+
+      subflows_[i].tcp_->mptcp_add_mapping (mcurseq_, sendbytes);
+      subflows_[i].tcp_->sendmsg (sendbytes);
+      mcurseq_ += sendbytes;
+
+      total_bytes_ -= sendbytes;
+    }
+    if (!slow_start)
+      calculate_alpha ();
+  }
+}
+
+/*
+ *  calculate alpha based on the equation in draft-raiciu-mptcp-congestion-01
+ */
+void
+MptcpAgent::calculate_alpha ()
+{
+  totalcwnd_ = alpha_ = 0;
+  double maxi = 0;
+  double sumi = 0;
+
+  for (int i = 0; i < sub_num_; i++) {
+    int cwnd = subflows_[i].tcp_->mptcp_get_cwnd ();
+
+    /* calculate smoothed cwnd */
+    if (subflows_[i].scwnd_ < 1)
+      subflows_[i].scwnd_ = cwnd;
+    else
+      subflows_[i].scwnd_ =
+        subflows_[i].scwnd_ * 0.875 + (double) cwnd *0.125;
+    double scwnd = subflows_[i].scwnd_;
+
+    totalcwnd_ += scwnd;
+
+    /* use smmothed RTT */
+    double rtt = subflows_[i].tcp_->mptcp_get_srtt ();
+    if (rtt < 0.000001)
+      continue;                 // too small
+
+    double tmpi = scwnd / (rtt * rtt);
+    if (maxi < tmpi)
+      maxi = tmpi;
+
+    sumi += scwnd / rtt;
+  }
+  if (!sumi)
+    return;
+  alpha_ = totalcwnd_ * maxi / (sumi * sumi);
+}
